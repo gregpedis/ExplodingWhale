f13d0a7d96f4f6a99830923a91026947fae2fad3
gregpedis <trafalgar1618@hotmail.com>
2024-01-11 19:16:56 +0100

    Bump self-analyzer dependency to 0.4.0

--- ExplodingWhale/ExplodingWhale/Rules/AnalyzerBase.cs
+++ ExplodingWhale/ExplodingWhale/Rules/AnalyzerBase.cs
@@ -7,17 +7,13 @@ namespace ExplodingWhale.Rules;
 
 public abstract class AnalyzerBase : DiagnosticAnalyzer
 {
+    public string DiagnosticId => $"{Definitions.RULE_PREFIX}{Id}";
+    public DiagnosticDescriptor Rule => DiagnosticDescriptorFactory.Create(DiagnosticId, Title, MessageFormat, GetType().Name);
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
     protected abstract string Id { get; }
     protected abstract string Title { get; }
     protected abstract string MessageFormat { get; }
-
-    public string DiagnosticId => $"{Definitions.RULE_PREFIX}{Id}";
-
-    public DiagnosticDescriptor Rule =>
-        DiagnosticDescriptorFactory.Create(DiagnosticId, Title, MessageFormat, GetType().Name);
-
-    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
-        ImmutableArray.Create(Rule);
 
     public override void Initialize(AnalysisContext context)
     {
